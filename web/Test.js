a07eeb379d7cdb5f70e720330c13b5a519e48a35
Nadheesh <nadheeshj@gmail.com>
2015-08-13 10:14:55 +0530

    merge conflicts

--- web/Test.js
+++ web/Test.js
@@ -5,3 +5,10 @@
  */
 
 
+QUnit.test("Testing Minus function", function(assert) {
+  assert.equal(5, minus(5, 3), "5 - 3 is 5");
+});
+
+QUnit.test("Testing Plus function with failing", function(assert) {
+  assert.equal(5, minus(2, 2), "2 + 2 is 5");
+});
